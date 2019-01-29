6cc83917f155d66d66faa852fbe4a1cab15cb300
itayhau <itayhau@gmail.com>
2019-01-29 09:10:39 +0200

    Add files via upload

---
+++ TutorialPoints2_DepProp/TutorialPointsDepProp/MyUserControl.xaml.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace TutorialPointsDepProp
+{
+    /// <summary>
+    /// Interaction logic for MyUserControl.xaml
+    /// </summary>
+    public partial class MyUserControl : UserControl
+    {
+        public MyUserControl()
+        {
+            InitializeComponent();
+        }
+
+            public static readonly DependencyProperty SetTextProperty =
+                DependencyProperty.Register("SetText", typeof(string), typeof(MyUserControl),
+                    new PropertyMetadata("", new PropertyChangedCallback(OnSetTextChanged)));
+            public string SetText
+            {
+                get { return (string)GetValue(SetTextProperty); }
+                set { SetValue(SetTextProperty, value); }
+            }
+            private static void OnSetTextChanged(DependencyObject d,
+                DependencyPropertyChangedEventArgs e)
+            {
+                MyUserControl UserControl1Control = d as MyUserControl;
+                UserControl1Control.OnSetTextChanged(e);
+            }
+            private void OnSetTextChanged(DependencyPropertyChangedEventArgs e)
+            {
+                tbTest.Text = e.NewValue.ToString();
+            }
+        }
+}
---
+++ TutorialPoints2_DepProp/readme.txt
@@ -0,0 +1,4 @@
+Creating Dependency property
+In the project need to create new UserControll called MyUserControl
+x:Type - smae as typeOf
+CLR property vs Dependency Property
