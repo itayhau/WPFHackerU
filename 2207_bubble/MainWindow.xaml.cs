8e19806c32e7206ab070f47932b4a7b46b6380de
itayhau <itayhau@gmail.com>
2019-07-24 10:28:42 +0300

    Add files via upload

---
+++ 2207_bubble/MainWindow.xaml.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Bubble
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            MessageBox.Show("Hi from the outter button!");
+        }
+
+        private void OuterElippse_MouseDown(object sender, MouseButtonEventArgs e)
+        {
+            this.Title = "Outer ellipsed has changed the title!";
+        }
+
+        private void InnerButton_Click(object sender, RoutedEventArgs e)
+        {
+            MessageBox.Show("I am the inner button!");
+        }
+
+        private void Button_PreviewMouseMove(object sender, MouseEventArgs e)
+        {
+            MessageBox.Show("Mouse move by the outter button"); 
+        }
+    }
+}
