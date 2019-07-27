6d05f75e1ff0438c249343e6f45123f1ddea34bd
itayhau <itayhau@gmail.com>
2019-07-27 23:09:31 +0300

    Add files via upload

---
+++ 2307_ICommand/Command.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Input;
+
+namespace WpfApp5
+{
+    public class Command : ICommand
+    {
+        // create also:  class ActionCommand<T> : ICommand
+
+        Action<object> executeMethod;
+
+        Func<bool> canExecuteMethod;
+
+        public Command(Action<object> executeMethod,  Func<bool> canExecuteMethod)
+        {
+            this.executeMethod = executeMethod;
+            this.canExecuteMethod = canExecuteMethod;
+        }
+
+        public bool CanExecute(object parameter)
+        {
+            // return canExecuteMethod(); 
+
+            // will be calculated only once
+            
+            return true;
+        }
+
+        public void Execute(object parameter)
+        {
+            executeMethod(parameter);
+        }
+
+    }
+}
---
+++ 2307_ICommand/MainWindowViewModel.cs
@@ -0,0 +1,61 @@
+﻿using Prism.Commands;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Input;
+
+namespace WpfApp5
+{
+    public class MainWindowViewModel
+    {
+        public ICommand  MyCommand { get; set; }
+
+        // NEED - Prism.wpf  NUGET
+        public DelegateCommand MyCommand2 { get; set; } // to change the can execute in real time
+
+        public MainWindowViewModel()
+        {
+            MyCommand = new Command(ExecuteMethod, CanExecuteMethod);
+
+            MyCommand2 = new DelegateCommand(ExecuteMethod2, CanExecuteMethod2);
+
+            Task.Run(() =>
+            {
+                while (true)
+                {
+                    //CommandManager.InvalidateRequerySuggested();
+                    MyCommand2.RaiseCanExecuteChanged();
+                    Thread.Sleep(500);
+                }
+            });
+        }
+
+        private void ExecuteMethod(object parameter)
+        {
+            MessageBox.Show("no code behind!");
+        }
+
+        private bool CanExecuteMethod()
+        {
+            return true;
+        }
+
+        // ==================================  DelegateCommand  ======================================================
+
+        private bool CanExecuteMethod2()
+        {
+            return DateTime.Now.Second % 2 == 0;
+        }
+
+
+        private void ExecuteMethod2()
+        {
+            MessageBox.Show("no code behind!");
+        }
+
+    }
+}
