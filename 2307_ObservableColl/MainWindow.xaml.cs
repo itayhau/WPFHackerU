cb26eb7fcfcc6d70a6158288a726346680d2f4ca
itayhau <itayhau@gmail.com>
2019-07-28 17:43:49 +0300

    Add files via upload

---
+++ 2307_ObservableColl/MainWindow.xaml.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace _2807
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        ObservableCollection<Person> persons = new ObservableCollection<Person>();
+        public MainWindow()
+        {
+            persons.Add(new Person() { Name = "a" });
+            persons.Add(new Person() { Name = "b" });
+            persons.Add(new Person() { Name = "c" });
+            InitializeComponent();
+
+            this.DataContext = persons;
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            persons.Add(new Person() { Name = "d" });
+        }
+    }
+
+    public class Person
+    {
+        public string Name { get; set; }
+    }
+}
