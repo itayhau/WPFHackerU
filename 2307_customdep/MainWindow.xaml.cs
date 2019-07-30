a9f58da437a9bdb8799b4c7ed70cb911089aa579
itayhau <itayhau@gmail.com>
2019-07-30 19:55:44 +0300

    Add files via upload

---
+++ 2307_customdep/MainWindow.xaml.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using System.Windows.Threading;
+
+namespace CustomDepProp
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+            EventHandler ev = (o, e) => {
+                SetValue(CounterProperty, Counter + 1);
+            };
+
+            DispatcherTimer timer = new DispatcherTimer(TimeSpan.FromSeconds(1), DispatcherPriority.Normal,
+                ev, Dispatcher);
+           
+            // you don't need it !!!
+           // this.DataContext = this;
+        }
+
+        public int Counter
+        {
+            get { return (int)GetValue(CounterProperty); }
+            set { SetValue(CounterProperty, value);}
+        }
+
+        public static readonly DependencyProperty CounterProperty =
+            DependencyProperty.Register("Counter", typeof(int), typeof(MainWindow), new PropertyMetadata(3));
+    }
+}
