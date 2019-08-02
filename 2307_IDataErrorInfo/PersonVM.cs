5c02e83433ca5c9d1bf35407397e527f67c134a6
itayhau <itayhau@gmail.com>
2019-08-02 21:40:23 +0300

    Add files via upload

---
+++ 2307_IDataErrorInfo/PersonVM.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+namespace WpfApp6
+{
+    public class PersonVM : INotifyPropertyChanged, IDataErrorInfo
+    {
+        private string mobileNumber;
+        public string MobileNumber
+        {
+            get { return mobileNumber; }
+            set
+            {
+                mobileNumber = value;
+                PropertyChanged(this, new PropertyChangedEventArgs("MobileNumber"));
+            }
+        }
+
+        private string name;
+        public string Name
+        {
+            get { return name; }
+            set
+            {
+                name = value;
+                PropertyChanged(this, new PropertyChangedEventArgs("Name"));
+            }
+        }
+
+        public string Error
+        {
+            get
+            {
+                return string.Empty;
+            }
+        }
+
+        public string this[string propertyName]
+        {
+            get
+            {
+                return GetErrorForProperty(propertyName);
+            }
+        }
+
+        private string GetErrorForProperty(string propertyName)
+        {
+            switch (propertyName)
+            {
+                case "MobileNumber":
+                    Regex regex = new Regex(@"^\D?(\d{3})\D?\D?(\d{3})\D?(\d{4})$");
+                    if (MobileNumber == null || regex.Match(MobileNumber) == Match.Empty)
+                        return "Invalid Phone Format";
+                    else return string.Empty;
+                default:
+                    return string.Empty;
+            }
+        }
+
+        public event PropertyChangedEventHandler PropertyChanged = delegate { };
+    }
+}
